b09a29820549710c9f1ac7a870ebf4a8e0e7445c
Dilawar Singh <dilawars@ncbs.res.in>
2015-07-02 15:02:05 +0530

    Test with new dependencies.

--- sbml_local_project.cmake
+++ sbml_local_project.cmake
@@ -1,5 +1,4 @@
 # This cmake script builds local sbml.
-include(ExternalProject)
 MESSAGE("++ Building local sbml")
 
 SET(LIBSBML_SRC_DIR ${PROJECT_SOURCE_DIR}/external/libsbml-5.11.4)
@@ -14,23 +13,14 @@ ADD_CUSTOM_COMMAND(OUTPUT _libsbml.out
     --with-libxml
     --with-zlib
     --with-bzip2 
+    COMMAND make 
     COMMAND make install
     WORKING_DIRECTORY ${LIBSBML_SRC_DIR}
     )
 
-ADD_CUSTOM_TARGET(_libsml ALL DEPENDS _libsbml.out)
+ADD_CUSTOM_TARGET(_libsml_local ALL DEPENDS _libsbml.out)
 set(LIBSBML_LIBRARY_LOCAL ${SBML_INSTALL_DIR}/lib/libsbml-static.a)
 MESSAGE("++ LIBSBML_LIBRARY: ${LIBSBML_LIBRARY_LOCAL}")
 include_directories(${SBML_INSTALL_DIR}/include)
-add_dependencies(moose-bin _libsbml)
-#add_dependencies(moose-python _libsml)
-
-#ExternalProject_Add(sbml_local
-    #PREFIX ${LIBSBML_SRC_DIR}
-    #SOURCE_DIR .
-    #CMAKE_ARGS -DBUILD_SHARED=OFF
-    #)
-
-#ExternalProject_Get_Property(sbml_local INSTALL_DIR)
-#include_directories(${INSTALL_DIR}/include)
-#SET(LIBSBML_LIBRARY_LOCAL ${INSTALL_DIR}/lib/libsbml.a)
+add_dependencies(moose-bin _libsbml_local)
+add_dependencies(_moose _libsbml_local)
